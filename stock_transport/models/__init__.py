6f2433fe50907ff391c42244732193abeabdfdfd
Yagnik Parmar <157007055+yagp-odoo@users.noreply.github.com>
2024-03-07 15:44:27 +0530

    Delete stock_transport directory

--- stock_transport/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import stock_fleet_category
-from . import stock_dock
-from . import stock_picking_batch
--- stock_transport/models/stock_dock.py
+++
@@ -1,6 +0,0 @@
-from odoo import models,fields,api,exceptions
-
-class StockFleet(models.Model):
-    _name = "stock.dock"
-
-    name = fields.Char()
